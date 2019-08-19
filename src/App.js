fb578209fa3b2ac14053662b61e560698bf2ee66
aliaksandr vishneuski <aliaksandr_vishneuski@senla.eu>
2019-08-19 19:28:59 +0300

    1. use effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import TodoList from './TodoList'
 
 
@@ -17,11 +17,23 @@ export default function App () {
 //передаем нач состояние к-е хотим использовать
 
     const [todos, setTodos] = useState([
-      {id: 1, title: 'First todo', completed: false},
-      {id: 2, title: 'Second todo', completed: true},
+      // {id: 1, title: 'First todo', completed: false},
+      // {id: 2, title: 'Second todo', completed: true},
     ]) 
-
     const [todoTitle, setTodoTitle] = useState('')
+
+    const handleClick = () => console.log('click') 
+    
+    
+    useEffect(()=>{
+      const raw = localStorage.getItem('todos') || []
+      setTodos(JSON.parse(raw))
+    }, [])
+
+    useEffect(()=>{
+      document.addEventListener('click', handleClick)
+      localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
 
     const addTodo = event => {
       if(event.key === 'Enter') {
