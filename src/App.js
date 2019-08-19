eba0e91165b4833610507f0339baf93539945f3a
aliaksandr vishneuski <aliaksandr_vishneuski@senla.eu>
2019-08-19 18:19:41 +0300

    1. use state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import TodoList from './TodoList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default function App () {
+  // state = {
+  //   todos: [
+  //     {id: 1, title: 'First todo', completed: false},
+  //     {id: 2, title: 'Second todo', completed: true},
+  //   ]
+  // }
+
+//как результат ф-и use state мы получаем массив из 2-х элементов:
+// 1-й элкмент -  состояние
+//2-й элемент -  . ф-я--setter к-я позволяем изменяет состояние
+//передаем нач состояние к-е хотим использовать
+
+    const [todos, setTodos] = useState([
+      {id: 1, title: 'First todo', completed: false},
+      {id: 2, title: 'Second todo', completed: true},
+    ]) 
+
+    const [todoTitle, setTodoTitle] = useState('')
+
+    const addTodo = event => {
+      if(event.key === 'Enter') {
+        setTodos([
+          ...todos,
+          {
+            id:Date.now(),
+            title: todoTitle,
+            completed: false 
+          }
+        ])
+        setTodoTitle('')
+      }
+    }
+
+    return (
+      <div className="container">
+        <h1>Todo app</h1>
+
+          <div className="input-field">
+            <input
+              type="text"
+              value = {todoTitle}
+              onChange = {event => setTodoTitle(event.target.value)}
+              onKeyPress = {addTodo}
+
+            />
+            <label>Todo name</label>
+          </div>
+
+          <TodoList todos={todos} />
+      </div>
+    );
 }
-
-export default App;
